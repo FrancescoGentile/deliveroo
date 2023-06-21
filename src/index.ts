720e7ae28a739e37109c86d1550ed4623bb97786
Francesco Gentile <francesco.gentile@studenti.unitn.it>
2023-06-21 15:45:30 +0200

    Completed first solution

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,25 @@
 //
 //
 
-function main() {
-  // eslint-disable-next-line no-console
-  console.log('Hello world!');
+import * as dotenv from 'dotenv';
+
+import { initInfrastructure } from './infrastructure';
+import { initAgent } from './domain';
+
+async function main() {
+  dotenv.config();
+
+  if (!process.env.HOST || !process.env.TOKEN) {
+    throw new Error('HOST and TOKEN must be set.');
+  }
+
+  const server = await initInfrastructure(process.env.HOST, process.env.TOKEN);
+  const agent = await initAgent(server);
+  await agent.run();
 }
 
-main();
+main()
+  // eslint-disable-next-line no-console
+  .then(() => console.log('Terminated.'))
+  // eslint-disable-next-line no-console
+  .catch((err: Error) => console.error(err));
